306a57917ae7af11e945043b50d1a6c8d0a6ff8e
Karen <kcazares0008@mymail.lausd.net>
2017-07-28 13:55:37 -0700

    Include

---
+++ Information, kinda/javascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+	$("#button-popup").click(function()
+	{
+		$(".window-popup").fadeIn(300);
+	});
+	$("#button-popup-close").click(function()
+	{
+		$(".window-popup").fadeOut(300);
+	});
+$("#button-popup1").click(function()
+	{
+		$(".window-popup1").fadeIn(300);
+	});
+	$("#button-popup-close1").click(function()
+	{
+		$(".window-popup1").fadeOut(300);
+	});
+
+
+});
